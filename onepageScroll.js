bb8e7444b433306b5400ea1d54f9a3d03b19cd59
ewice <henning.loewe@hotmail.de>
2020-02-07 20:02:07 +0100

    use scrollIntoView

--- onepageScroll.js
+++ onepageScroll.js
@@ -1,13 +1,39 @@
 let lastScroll = 0;
+let currentSection = 0;
+let ticking = false;
 
-window.addEventListener('scroll', function() {
-  // scroll down
-  if (lastScroll < window.pageYOffset) {
-    window.scrollBy(0, window.innerHeight);
+const sections = document.getElementsByClassName('section');
+console.log("TCL: sections", sections);
+
+window.onload = function get_body() {
+  body = document.getElementsByTagName('body')[0];
+  body.classList.add('page-viewing-01');
+}
+
+const scrollSectionIntoView = () => {
+  if (lastScroll < window.pageYOffset && currentSection < sections.length-1) {
+    currentSection++;
+    sections[currentSection].scrollIntoView();
   }
-  // scroll up
-  else if (lastScroll > window.pageYOffset) {
-    window.scrollBy(0, window.innerHeight * -1);
+  else if (lastScroll > window.pageYOffset && currentSection > 0) {
+    currentSection--;
+    sections[currentSection].scrollIntoView();
   }
+  console.log('CurrentSection', currentSection);
   lastScroll = window.pageYOffset;
+}
+
+// window.addEventListener('scroll', () => {
+//     if (lastScroll < window.pageYOffset) {
+//       window.scrollBy(0, window.innerHeight);
+//     }
+//     else if (lastScroll > window.pageYOffset) {
+//       window.scrollBy(0, window.innerHeight * -1);
+//     }
+//     lastScroll = window.pageYOffset;
+//     console.log("TCL: window.pageYOffset", window.pageYOffset)
+// });
+
+window.addEventListener('scroll', () => {
+  scrollSectionIntoView();
 });
